5b965d9642c197f0ec51d2ac0c7e049831aaccc2
nk243547 <nk243547@gmail.com>
2023-03-17 15:51:01 -0700

    printing alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,19 +7,19 @@
 */
 
 int main(void)
-{
-int lowercase = 'a';
-int uppercase = 'A';
-while (lowercase <= 'z')
-{
-putchar(lowercase);
-lowercase += 1;
-}
-while (uppercase <= 'Z')
-{
-putchar(uppercase);
-uppercase +=1;
-}
+{       
+int lowerCase = 'a';
+int upperCase = 'A';
+while (lowerCase <= 'z')
+{       
+putchar(lowerCase);
+lowerCase += 1;
+}       
+while (upperCase <= 'Z')
+{       
+putchar(upperCase);
+upperCase +=1;
+}       
 putchar('\n');
 return (0);
 }
