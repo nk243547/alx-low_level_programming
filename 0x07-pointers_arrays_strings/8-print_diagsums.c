1c23f68721eb7bd72a3ff73c8072422a71d5ca52
nk243547 <nk243547@gmail.com>
2023-04-04 07:14:06 -0700

    8-print_diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -14,10 +14,9 @@ int sum1 = 0, sum2 = 0;
 
 for (i = 0; i < size; i++)
 {
-sum1 += *(a + i * size + i);  /* sum of the elements on the main diagonal*/
-sum2 += *(a + i * size + (size - i - 1));  /* sum of the elements on the secondary diagonal*/
+sum1 += *(a + i * size + i);
+sum2 += *(a + i * size + (size - i - 1));
 }
 
-printf("Sum of main diagonal elements: %d\n", sum1);
-printf("Sum of secondary diagonal elements: %d\n", sum2);
+printf("%d, %d\n", sum1, sum2);
 }
