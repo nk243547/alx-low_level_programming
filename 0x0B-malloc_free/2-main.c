065998b5fd4c6c5d26c1995be37011315f299e19
nk243547 <nk243547@gmail.com>
2023-04-11 14:25:52 -0700

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include<stdio.h>
+#include<stdlib.h>
+char *str_concat(char *s1, char *s2);
+
+int main(void)
+{
+char *s;
+
+s = str_concat("Betty", "Holberton");
+
+if(s == NULL)
+{
+printf("failed\n");
+return (1);
+}
+printf("%s\n", s);
+free(s);
+return (0);
+}
