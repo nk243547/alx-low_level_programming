73009f03d814a2ca400a018925eb24585421a86c
nk243547 <nk243547@gmail.com>
2023-04-17 14:34:33 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,6 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-#define ABS(x) (((x) < 0) ? (x * -1) : (x))
-#endif
+#ifndef _function_like_macro_h_
+#define _function_like_macro_h_
+
+#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
+
+#endif /* _function_like_macro_h_ */
