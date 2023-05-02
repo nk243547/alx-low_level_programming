abcf3dc4e5d3f069abae590106a0c065a95469f5
nk243547 <nk243547@gmail.com>
2023-05-02 13:04:29 -0700

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,13 +1,29 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
-* free_listint - define function
-* @head: describe argument
+* add_nodeint_end - add new nodes to the end of the list
+* @head: current place in the list
+* @n: int to add to the list
+* Return: pointer to current position in list
 */
-void free_listint(listint_t *head)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-if (head == NULL)
-return;
-free_listint(head->next);
-free(head);
+listint_t *new, *current;
+
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+return (NULL);
+new->n = n;
+new->next = NULL;
+
+if (*head == NULL)
+{
+*head = new;
+return (*head);
+}
+current = *head;
+while (current->next != NULL)
+current = current->next;
+current->next = new;
+return (*head);
 }
